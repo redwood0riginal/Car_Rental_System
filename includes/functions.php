1cbd2da8bb50ee08fc7345f7950f7461511a2e22
Badr <moslihbadr2@gmail.com>
2023-06-26 00:30:27 +0100

    add head function

---
+++ includes/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+// head function
+function head($title) {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+    <link rel="stylesheet" href="./css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="./css/style.css"/>
+    <title>$title</title>
+  </head>
+  `;
+}
