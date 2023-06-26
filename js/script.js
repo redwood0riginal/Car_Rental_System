a173cb75b221e2b50032a658914e38a66acd4a30
Badr <moslihbadr2@gmail.com>
2023-06-26 23:25:01 +0100

    add eye toggle for password

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(()=>{
+
+  // toggle eye password
+  $('.eye').on('click', (e) => {
+    if ($(e.currentTarget).siblings("input").attr("type") === "password") {
+      $(e.currentTarget).attr('class', 'fa-solid fa-eye-slash eye');
+      $(e.currentTarget).siblings("input").attr('type', 'text');
+    }else {
+      $(e.currentTarget).attr('class', 'fa-solid fa-eye eye');
+      $(e.currentTarget).siblings("input").attr('type', 'password');
+    }
+  })
+
+})
