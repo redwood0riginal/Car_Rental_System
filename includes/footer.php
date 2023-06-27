def81c85088ebf0752a94b4e971eaf68b7267b52
Badr <moslihbadr2@gmail.com>
2023-06-27 23:43:41 +0100

    update footer.php

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<!-- footer markup goes here -->
+
+
+
+
+<!-- js scripts -->
+<script src="./js/jquery.min.js"></script>
+<script src="./js/bootstrap.min.js"></script>
+<script src="./js/script.js"></script>
