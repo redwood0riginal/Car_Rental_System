2486cb11b283772184e09e2959b71472ac45290c
Ahmed Tiguini <130414983+redwood0riginal@users.noreply.github.com>
2023-07-03 16:20:57 +0100

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,43 @@
 <!-- footer markup goes here -->
-
+<footer>
+        <div class="info">
+            <img src="Car_Rental_Logo.png" alt="">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            <p>06 54 54 87 62</p>
+            <a href="">rentalcar@gmail.com</a>
+        </div>
+        <div class="navigation">
+            <h2>Navigation</h2>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Blog</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+        <div class="services">
+            <h2>Services</h2>
+            <ul>
+                <li>Drone Mapping</li>
+                <li>Real State</li>
+                <li>Commercial</li>
+                <li>Construcion</li>
+            </ul>
+        </div>
+        <div class="subscribe">
+            <h2>Subscribe newsletter</h2>
+            <p>Subscribe our newsletter to get updates about our sevices and offers. </p>
+            <input type="email" name="" placeholder="Email Adress"><i class="fa-solid fa-arrow-right"></i>
+            <ul>
+                <li><i class="fa-brands fa-linkedin"></i></li>
+                <li><i class="fa-brands fa-facebook"></i></li>
+                <li><i class="fa-brands fa-instagram"></i></li>
+                <li><i class="fa-brands fa-twitter"></i></li>
+            </ul>
+        </div>
+    </footer>
+    <p class="copy">Copyright ©2023 All rights reserved | <span>CAR-RENTAL MT</span> </p>
 
 
 
