ee102b5469e4f287991edecac97b0aef94262862
Ahmed Tiguini <130414983+redwood0riginal@users.noreply.github.com>
2023-07-03 16:19:34 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,26 @@
 <!-- header markup goes here -->
+    <header>
+        <div class="logo">
+            <img src="Car_Rental_Logo.png" alt="">
+        </div>
+        <div class="nav">
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="cars.php">Cars</a></li>
+                <li><a href="companies.php">Companies</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="profile.php">Profile</a></li>
+            </ul>
+
+        </div>
+        <div class="btns">
+            <input type="button" value="Log In">
+            <input type="button" value="Sign Up">
+        </div>
+        <div class="dropdown">
+            <i class="fa-solid fa-bars"></i>
+        </div>
+    </header>
 
 
 
